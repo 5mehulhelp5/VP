2298f60827805a923d4cc38741885749c513ab13
Viking Production <154587935+VikingProduction@users.noreply.github.com>
2025-06-01 20:29:27 +0200

    Create requirejs-config.js

---
+++ CoreFinder/view/frontend/requirejs-config.js
@@ -0,0 +1,14 @@
+/**
+ * Module : VP_CoreFinder
+ * Fichier : view/frontend/requirejs-config.js
+ * Rôle : Déclare l’alias JS pour l’initialisation des facettes
+ * Auteur : Auzoult Marc-Antoine
+ * Date : 2025-06-01
+ */
+var config = {
+    map: {
+        '*': {
+            'coreFinderFacets': 'VP_CoreFinder/js/facets'
+        }
+    }
+};
