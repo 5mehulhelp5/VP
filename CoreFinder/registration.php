9aacf37ab19f2b8eb79e925b94267b5a7162a5c5
Viking Production <154587935+VikingProduction@users.noreply.github.com>
2025-06-01 20:00:20 +0200

    Create registration.php

---
+++ CoreFinder/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Module : VP_CoreFinder
+ * Fichier : registration.php
+ * Rôle : Déclaration du module dans Magento 2
+ * Auteur : Auzoult Marc-Antoine
+ * Date : 2025-06-01
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'VP_CoreFinder',
+    __DIR__
+);
